10564cee64783e322d62c8ac577bb7354aa9f745
Simon Tse <stse@slack-corp.com>
2020-09-19 10:49:24 -0700

    Four buttons do slightly different ones

--- app/src/main/java/com/simon/waltest/MainActivity.kt
+++ app/src/main/java/com/simon/waltest/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -18,9 +17,11 @@ import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import timber.log.Timber
 import java.text.DateFormat
 import java.util.*
 
+private const val DELAY_DB_WRITE_MS = 3000L
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,8 +44,40 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding.root)
     setSupportActionBar(binding.toolbar)
 
-    binding.fab.setOnClickListener {
-      testWriteAndReadFromDifferentWritableDatabase()
+    binding.mainContent.diffDbWTrans.setOnClickListener {
+      testWriteAndReadFromDifferentWritableDatabase(
+        DELAY_DB_WRITE_MS,
+        dbHelper.writableDatabase,
+        dbHelper.writableDatabase
+      )
+    }
+
+    binding.mainContent.sameDbWTrans.setOnClickListener {
+      val db = dbHelper.writableDatabase
+      testWriteAndReadFromDifferentWritableDatabase(
+        DELAY_DB_WRITE_MS,
+        db,
+        db
+      )
+    }
+
+    binding.mainContent.diffDbWithoutTrans.setOnClickListener {
+      testWriteAndReadFromDifferentWritableDatabase(
+        DELAY_DB_WRITE_MS,
+        dbHelper.writableDatabase,
+        dbHelper.writableDatabase,
+        false
+      )
+    }
+
+    binding.mainContent.sameDbWithoutTrans.setOnClickListener {
+      val db = dbHelper.writableDatabase
+      testWriteAndReadFromDifferentWritableDatabase(
+        DELAY_DB_WRITE_MS,
+        db,
+        db,
+        false
+      )
     }
   }
 
@@ -64,42 +97,65 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
-  private fun testWriteAndReadFromDifferentWritableDatabase() {
+  private fun testWriteAndReadFromDifferentWritableDatabase(
+    delay: Long,
+    writingDatabase: SupportSQLiteDatabase,
+    readingDatabase: SupportSQLiteDatabase,
+    readShouldUseTransaction: Boolean = true
+  ) {
     compositeDisposable.add(
       Completable.fromCallable {
-        val db = dbHelper.writableDatabase
+        val threadName = Thread.currentThread().name
+        val db = writingDatabase
+        Timber.d("$threadName write about to start transaction")
+
         db.beginTransactionNonExclusive()
 
         insertValue(db, currentTimeString())
 
-        Thread.sleep(3000)
+        if (delay > 0) {
+          Timber.d("$threadName write going to sleep for $delay ms")
+          Thread.sleep(delay)
+        }
 
         db.setTransactionSuccessful()
         db.endTransaction()
+        Timber.d("$threadName write transaction done")
       }
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(
           { Snackbar.make(rootView, "Done Write", Snackbar.LENGTH_SHORT).show() },
-          { Log.e(null, "Errored for writing", it) }
+          { Timber.e(it, "Errored for writing") }
         )
     )
 
     compositeDisposable.add(
       Single.fromCallable {
-        val db = dbHelper.writableDatabase
-        db.beginTransactionNonExclusive()
-        val text = readValue(db)
-        db.setTransactionSuccessful()
-        db.endTransaction()
+        val threadName = Thread.currentThread().name
+        val db = readingDatabase
 
-        text
+        if (readShouldUseTransaction) {
+          Timber.d("$threadName read about to start transaction")
+
+          db.beginTransactionNonExclusive()
+          val text = readValue(db)
+          db.setTransactionSuccessful()
+          db.endTransaction()
+
+          Timber.d("$threadName read transaction done")
+          text
+        } else {
+          Timber.d("$threadName read start - no transaction")
+          readValue(db)
+          Timber.d("$threadName read done - no transaction")
+        }
       }
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(
           { Snackbar.make(rootView, "Done Read - $it", Snackbar.LENGTH_SHORT).show() },
-          { Log.e(null, "Errored for writing", it) }
+          { Timber.e(it, "Errored for writing") }
         )
     )
   }
